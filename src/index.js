7b468539df8c6fa3f9e70c0b251b23f06dbf218c
fd-mailden <79415152+fd-mailden@users.noreply.github.com>
2021-05-12 22:04:17 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./scss/index.css";
+import "./scss/carousel.css";
+import "./scss/index.scss";
+import "./assets/images/123.png";
+import "./assets/images/n1.jpg";
+import "./assets/images/n2.jpg";
+import "./assets/images/n3.jpg";
+import "./assets/images/news.jpg";
+console.log('Hello World from your main file! ' + process.env.APP_NAME);
+
